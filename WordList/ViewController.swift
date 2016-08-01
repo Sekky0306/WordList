e4f68e6e5f2619270f2f309c3e4b643cb254887c
Sekky <step1336@gmail.com>
2016-08-01 17:19:44 +0900

    Initial Commit

---
+++ WordList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WordList
+//
+//  Created by 関戸優紀 on 2016/08/01.
+//  Copyright (c) 2016年 関戸優紀. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WordListTests/WordListTests.swift
@@ -0,0 +1,36 @@
+//
+//  WordListTests.swift
+//  WordListTests
+//
+//  Created by 関戸優紀 on 2016/08/01.
+//  Copyright (c) 2016年 関戸優紀. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class WordListTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
